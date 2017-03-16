1d1116f6cdcbd9c2e89fed4d21940a7e46f818ea
jynthia <cinthialgs@gmail.com>
2017-03-16 15:23:11 -0300

    Add external helper method for filling the title in the layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	# adds title
+  def fill_title(page_title = '')
+    base_title = "Reclama App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " - " + base_title
+    end
+  end
 end
