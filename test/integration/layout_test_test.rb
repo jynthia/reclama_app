d376507016ad03a4958f1f5f412675df9bd333c9
jynthia <cinthialgs@gmail.com>
2017-03-16 16:57:04 -0300

    Update layouts integration test

--- test/integration/layout_test_test.rb
+++ test/integration/layout_test_test.rb
@@ -1,10 +1,12 @@
 require 'test_helper'
 
 class LayoutTestTest < ActionDispatch::IntegrationTest
-  get root_path
-  assert_template 'first/home'
-  assert_select "a[href=?]", root_path
-  assert_select "a[href=?]", help_path
-  assert_select "a[href=?]", about_path
-  assert_select "a[href=?]", submit_path, count: 2
+	test "links" do
+  		get root_path
+  		assert_template 'first/home'
+  		assert_select "a[href=?]", root_path
+  		assert_select "a[href=?]", help_path
+  		assert_select "a[href=?]", about_path
+  		assert_select "a[href=?]", submit_path, count: 2
+  	end
 end
