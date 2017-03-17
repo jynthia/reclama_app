a4a803118ff414c41772b3bf44acb0a6e315d0f8
jynthia <cinthialgs@gmail.com>
2017-03-16 21:48:36 -0300

    Added actions in suggestions controller

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -1,4 +1,30 @@
 class SuggestionsController < ApplicationController
+
   def new
+  	@message = Message.new
   end
+
+  def show
+  	@message = Message.find(params[:id])
+  end
+
+  def create
+  	@message = Message.new(message_params)
+  	if @message.save
+  		redirect_to @message 
+  	else
+  		render "new"
+  	end
+  end
+
+  def edit
+  	@message = Message.find(params[:id])
+  end
+
+  private
+
+  	def message_params
+  		params.require(:message).permit(:content, :number)
+  	end
+
 end
